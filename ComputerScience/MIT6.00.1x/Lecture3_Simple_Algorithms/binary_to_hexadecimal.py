2605232302698431f6085448ffeb6b63c6222575
rubengr16 <ruben16gallego@gmail.com>
2021-02-07 18:28:08 +0100

    minor changes binary to hex

--- ComputerScience/MIT6.00.1x/Lecture3_Simple_Algorithms/binary_to_hexadecimal.py
+++ ComputerScience/MIT6.00.1x/Lecture3_Simple_Algorithms/binary_to_hexadecimal.py
@@ -1,7 +1,14 @@
 # Given an integer through input, it converts it to decimal
 bin_num = input('Enter a binary integer to convert to hexadecimal: ')
 print(bin_num, 'in hexadecimal is', end=' ')
+is_neg = None
 resul = ''
+
+if bin_num[0] == '-':
+    is_neg = True
+    bin_num = bin_num[1:]
+else:
+    is_neg = False
 
 while len(bin_num) > 0:
     if len(bin_num) > 4:
@@ -10,30 +17,42 @@ while len(bin_num) > 0:
     else:
         act = '0' * (4 - len(bin_num)) + bin_num
         bin_num = ''
-    if act[0] == '0':  # Numbers of the form 0XXX, form 0 to 7 in hexadecimal
-        n = 0
-        for i in range(4):
-            n += int(act[3 - i]) * (2 ** i)
-        n = str(n)
-    else:
-        if act[1] == '0':
-            if act[2] == '0':  # Numbers 100X, 8 or 9 in hexadecimal
-                n = str(8 + int(act[0]) * (2 ** 1))
-            else:
-                if act[3] == '0':  # 1010, A in hexadecimal
-                    n = 'A'
-                else:  # 1011, B in hexadecimal
-                    n = 'B'
-        else:
-            if act[2] == '0':
-                if act[3] == '0':  # 1100, C
-                    n = 'C'
-                else:  # 1101, D
-                    n = 'D'
-            else:
-                if act[3] == 0:
-                    n = 'E'
-                else:
-                    n = 'F'
+    n = None
+    # Creation of a switch for each of the 16 possible hexadecimal numbers
+    if act == '0000':
+        n = '0'
+    elif act == '0001':
+        n = '1'
+    elif act == '0010':
+        n = '2'
+    elif act == '0011':
+        n = '3'
+    elif act == '0100':
+        n = '4'
+    elif act == '0101':
+        n = '5'
+    elif act == '0110':
+        n = '6'
+    elif act == '0111':
+        n = '7'
+    elif act == '1000':
+        n = '8'
+    elif act == '1001':
+        n = '9'
+    elif act == '1010':
+        n = 'A'
+    elif act == '1011':
+        n = 'B'
+    elif act == '1100':
+        n = 'C'
+    elif act == '1101':
+        n = 'D'
+    elif act == '1110':
+        n = 'E'
+    elif act == '1111':
+        n = 'F'
     resul = n + resul
+
+if is_neg:
+    resul = '-' + resul
 print(resul)
