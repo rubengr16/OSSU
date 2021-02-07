0251ea53f1b6a9561438263167e591324e114587
rubengr16 <ruben16gallego@gmail.com>
2021-02-07 18:54:36 +0100

    hex to bin

---
+++ ComputerScience/MIT6.00.1x/Lecture3_Simple_Algorithms/hexadecimal_to_binary.py
@@ -0,0 +1,38 @@
+# Given an integer through input, it converts it to decimal
+hex_num = input('Enter a hexadecimal integer to convert to binary: ')
+print(hex_num, 'in binary is', end=' ')
+is_neg = None
+resul = ''
+
+if hex_num[0] == '-':
+    is_neg = True
+    hex_num = hex_num[1:]
+else:
+    is_neg = False
+
+for c in hex_num:
+    if c.isdigit():
+        c = int(c)
+        aux = ''
+        while c > 0:
+            aux = str(c % 2) + aux  # Process for conversion
+            c //= 2
+        resul += ('0' * (4 - len(aux)) + aux)
+    else:
+        if c == 'A':
+            c = '1010'
+        elif c == 'B':
+            c = '1011'
+        elif c == 'C':
+            c = '1100'
+        elif c == 'D':
+            c = '1101'
+        elif c == 'E':
+            c = '1110'
+        elif c == 'F':
+            c = '1111'
+        resul += c
+
+if is_neg:
+    resul = '-' + resul
+print(resul)
