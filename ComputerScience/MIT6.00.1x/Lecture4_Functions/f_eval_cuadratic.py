2ddaa298788d44ab6bc69ea0fd111b9701662a1e
rubengr16 <ruben16gallego@gmail.com>
2021-02-08 02:15:21 +0100

    2 functions

---
+++ ComputerScience/MIT6.00.1x/Lecture4_Functions/f_eval_cuadratic.py
@@ -0,0 +1,7 @@
+# Python function, evalQuadratic(a, b, c, x), that returns the value of the quadratic  a*x^2 + b*x + c
+def evalQuadratic(a, b, c, x):
+    '''
+    a, b, c: numerical values for the coefficients of a quadratic equation
+    x: numerical value at which to evaluate the quadratic.
+    '''
+    return a * x ** 2 + b * x + c
---
+++ ComputerScience/MIT6.00.1x/Lecture4_Functions/f_square.py
@@ -0,0 +1,6 @@
+# Python function, square, that takes in one number and returns the square of that number
+def square(x):
+    '''
+    x: int or float.
+    '''
+    return x ** 2
